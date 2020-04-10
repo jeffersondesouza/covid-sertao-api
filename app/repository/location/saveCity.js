ae58647135b182987a5d1cd658a2807ee0b0145b
jeffersondesouza <jeffersondesouzacordel@gmail.com>
2020-04-10 17:49:27 -0300

    reports ok

--- app/repository/location/saveCity.js
+++ app/repository/location/saveCity.js
@@ -3,7 +3,16 @@ const mongoose = require('mongoose');
 const Location = mongoose.model('locations');
 
 const saveCity = async (params) => {
-  const { stateId, name, site, email, population, phones, logo } = params;
+  const {
+    stateId,
+    name,
+    site,
+    email,
+    population,
+    phones,
+    logo,
+    region,
+  } = params;
 
   return new Location({
     _parent: stateId,
@@ -14,6 +23,18 @@ const saveCity = async (params) => {
     phones,
     logo,
     regionType: '6',
+    report: {
+      confirmed: 0,
+      negative: 0,
+      suspects: 0,
+      deaths: 0,
+      recovered: 0
+    },
+    region: {
+      ...region,
+      uf: stateId,
+      country: '5e90d1d01c9d4400000e879f',
+    },
   }).save();
 };
 
