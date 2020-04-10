db99bd4b844734d0a861ed02c5964037b5ef69c7
jeffersondesouza <jeffersondesouzacordel@gmail.com>
2020-04-10 15:22:26 -0300

    lists ok

--- app/controllers/reports/byCity.js
+++ app/controllers/reports/byCity.js
@@ -1,6 +1,14 @@
 /* eslint-disable no-underscore-dangle */
 const LocationRepository = require('../../repository/location');
 
+const emptyReports = {
+  confirmed: 0,
+  suspects: 0,
+  negative: 0,
+  deaths: 0,
+  recovered: 0,
+};
+
 module.exports = async (req, res) => {
   try {
     const city = await LocationRepository.getUfCity({
@@ -8,7 +16,7 @@ module.exports = async (req, res) => {
       uf: req.params.uf,
     });
 
-    const { _id, name, report, updateAt, population } = city;
+    const { _id, name, report = emptyReports, updateAt, population } = city;
 
     res.send({
       id: _id,
