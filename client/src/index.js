63a0480115d2f1076ad04e92b154c041eba5f33d
jeffersondesouza <jeffersondesouzacordel@gmail.com>
2020-04-11 08:37:25 -0300

    routes

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+import { Routes } from './containers';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Routes />
   </React.StrictMode>,
   document.getElementById('root')
 );
